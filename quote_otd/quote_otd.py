f7672461fff7edbe2b62c81791b201979f203009
Wesley Reed <reed.wesley.s@gmail.com>
2024-06-16 22:43:31 -0400

    ugh

--- quote_otd/quote_otd.py
+++ quote_otd/quote_otd.py
@@ -153,6 +153,7 @@ class QuoteOfTheDay(commands.Cog):
             if guild_data["enabled"] and guild_data["post_time"]:
                 guild_data = await self.config.guild(guild).all()
                 channel = guild.get_channel(guild_data["channel_id"])
+                channel.send(f"Posting quote... {guild_data['post_time']}")
                 post_time = datetime.strptime(guild_data["post_time"], "%H:%M").time()
                 channel.send(f"Posting quote... {post_time}")
                 now = datetime.now().time()
